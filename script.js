9a7f33ce3ed8d086c8a49934f81c7486b78245f8
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-24 14:31:51 +0100

    Update getBlocks() to accept parameter blocksPerSide and dynamically size blocks to fit sketchPad

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // Everything MUST happen inside the fuction. Otherwise, the script keeps going back to the original node to append the child, overwriting whatever it has already created. You cannot generate more than one additional div that way!
 
-function getBlocks (blockNumber) {
+function getBlocks (blocksPerSide) {
+    let blockNumber = blocksPerSide * blocksPerSide;
+    // let blockHeight = 
     
     for (let i = 0; i < blockNumber; i++) {
-        var div = document.createElement("div");
+        let div = document.createElement("div");
+        div.style.width = 100/blocksPerSide +"%"
         document.getElementById("sketchPad").appendChild(div);
     }
 
 }
 
-getBlocks(256);
+getBlocks(16);
 
 
