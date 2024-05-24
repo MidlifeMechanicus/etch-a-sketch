05142c2eacd571ca58d305484fad745df2a088b1
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-24 13:47:24 +0100

    expand getBlocks function to accept parameter blockNumber

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 // Everything MUST happen inside the fuction. Otherwise, the script keeps going back to the original node to append the child, overwriting whatever it has already created. You cannot generate more than one additional div that way!
 
-function getBlocks () {
-    var div = document.createElement("div");
-    document.getElementById("sketchPad").appendChild(div);
+function getBlocks (blockNumber) {
+    
+    for (let i = 0; i < blockNumber; i++) {
+        var div = document.createElement("div");
+        document.getElementById("sketchPad").appendChild(div);
+    }
+
 }
 
-getBlocks();
-getBlocks();
-getBlocks();
-getBlocks();
-getBlocks();
-getBlocks();
+getBlocks(256);
 
 
-
-
-
-
-
-
-function generateGrid() {
-    var div = document.createElement('div');
-    document.getElementById("grid").appendChild(div);
-   }
-
-   generateGrid();
-   generateGrid();
-   generateGrid();
-   generateGrid();
